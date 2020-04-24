fc8c54b387f837a14cfc7bbb49841dffdfa19ac8
Mohamed-Omran <kamelomran3@gmail.com>
2020-04-24 02:05:49 +0200

    Correcting RK4

--- DoublePendulum.cpp
+++ DoublePendulum.cpp
@@ -15,52 +15,46 @@ void DoublePendulum::setPosition(sf::Vector2f pos) {
 // this function needs a thorough look for potential optimization
 void DoublePendulum::update_RK4() {
 
-
+    // potential energy
     double potential = -(m1 + m2) * g * l1 * cos(theta1) - m2 * g * l2 * cos(theta2);
+
+    // Kinetic energy 
     double kinetic = 0.5 * m1 * l1 * l1 * omega1 * omega1 + 0.5 * m2 * ((l1 * l1 * omega1 * omega1) + (l2 * l2 * omega2 * omega2) + 2 * l1 * l2 * omega1 * omega2 * cos(theta1 - theta2));
-    cout << kinetic <<"  "<< potential<< "  " <<kinetic + potential<< endl;
-    // Calculate Omega1
-    // these four variable are the K's of RK4
-        double I0 =(-g * (2 * m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (pow(omega2, 2) * l2 + pow(omega1, 2) * l1 * cos(theta1 - theta2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
 
-        double I1 =(-g * (2 * m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (pow(omega2, 2) * l2 + pow((omega1 + stepSize * 0.5 * I0), 2) * l1 * cos(theta1 - theta2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+    //displaying the kinetic, potential and total energy of the system.
+    cout << kinetic << "  " << potential << "  " << kinetic + potential << endl;
+    /////////////////////////////////////////////////////////////////////////////////////////////////
 
-        double I2 =(-g * (2 * m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (pow(omega2, 2) * l2 + pow((omega1 + stepSize * 0.5 * I1), 2) * l1 * cos(theta1 - theta2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+    // THE NEXT 16 VARIABLES ARE THE k's of Runge-Kutta for the 4 ODEs
+    // All K1
+    double dTheta1_1 = omega1;
+    double dOmega1_1 = (-g * (2 * m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (pow(omega2, 2) * l2 + pow(omega1, 2) * l1 * cos(theta1 - theta2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+    double dTheta2_1 = omega2;
+    double dOmega2_1 = (2 * sin(theta1 - theta2) * (pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + pow(omega2, 2) * l2 * m2 * cos(theta1 - theta2))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
 
-        double I3 =(-g * (2 * m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (pow(omega2, 2) * l2 + pow((omega1 + stepSize * I2), 2) * l1 * cos(theta1 - theta2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+    // All K2
+    double dTheta1_2 = omega1 + 0.5 * stepSize * dTheta1_1;
+    double dOmega1_2 = (-g * (2 * m1 + m2) * sin(theta1 + 0.5 * stepSize * dTheta1_1) - m2 * g * sin((theta1 + 0.5 * stepSize * dTheta1_1) - 2 * (theta2 + 0.5 * stepSize * dTheta2_1)) - 2 * sin((theta1 + 0.5 * stepSize * dTheta1_1) - (theta2 + 0.5 * stepSize * dTheta2_1)) * m2 * (pow(omega2 + 0.5 * stepSize * dOmega2_1, 2) * l2 + pow(omega1 + 0.5 * stepSize * dOmega1_1, 2) * l1 * cos((theta1 + 0.5 * stepSize * dTheta1_1) - (theta2 + 0.5 * stepSize * dTheta2_1)))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + 0.5 * stepSize * dTheta1_1) - 2 * (theta2 + 0.5 * stepSize * dTheta2_1))));
+    double dTheta2_2 = omega2 + 0.5 * stepSize * dTheta2_1;
+    double dOmega2_2 = (2 * sin((theta1 + 0.5 * stepSize * dTheta1_1) - (theta2 + 0.5 * stepSize * dTheta2_1)) * (pow(omega1 + 0.5 * stepSize * dOmega1_1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1 + 0.5 * stepSize * dTheta1_1) + pow(omega2 + 0.5 * stepSize * dOmega2_1, 2) * l2 * m2 * cos((theta1 + 0.5 * stepSize * dTheta1_1) - (theta2 + 0.5 * stepSize * dTheta2_1)))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + 0.5 * stepSize * dTheta1_1) - 2 * (theta2 + 0.5 * stepSize * dTheta2_1))));
 
-        double omega1New = omega1 + (stepSize/ 6.0) * (I0 + 2 * I1 + 2 * I2 + I3);
+    // All K3
+    double dTheta1_3 = omega1 + 0.5 * stepSize * dTheta1_2;
+    double dOmega1_3 = (-g * (2 * m1 + m2) * sin(theta1 + 0.5 * stepSize * dTheta1_2) - m2 * g * sin((theta1 + 0.5 * stepSize * dTheta1_2) - 2 * (theta2 + 0.5 * stepSize * dTheta2_2)) - 2 * sin((theta1 + 0.5 * stepSize * dTheta1_2) - (theta2 + 0.5 * stepSize * dTheta2_2)) * m2 * (pow(omega2 + 0.5 * stepSize * dOmega2_2, 2) * l2 + pow(omega1 + 0.5 * stepSize * dOmega1_2, 2) * l1 * cos((theta1 + 0.5 * stepSize * dTheta1_2) - (theta2 + 0.5 * stepSize * dTheta2_2)))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + 0.5 * stepSize * dTheta1_2) - 2 * (theta2 + 0.5 * stepSize * dTheta2_2))));
+    double dTheta2_3 = omega2 + 0.5 * stepSize * dTheta2_2;
+    double dOmega2_3 = (2 * sin((theta1 + 0.5 * stepSize * dTheta1_2) - (theta2 + 0.5 * stepSize * dTheta2_2)) * (pow(omega1 + 0.5 * stepSize * dOmega1_2, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1 + 0.5 * stepSize * dTheta1_2) + pow(omega2 + 0.5 * stepSize * dOmega2_2, 2) * l2 * m2 * cos((theta1 + 0.5 * stepSize * dTheta1_2) - (theta2 + 0.5 * stepSize * dTheta2_2)))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + 0.5 * stepSize * dTheta1_2) - 2 * (theta2 + 0.5 * stepSize * dTheta2_2))));
 
-    
-    // Calculate theta1
-    // these four variable are the K's of RK4
-    double k0 = omega1;
-    double k1 = omega1 + stepSize * 0.5 * k0;
-    double k2 = omega1 + stepSize * 0.5 * k1;
-    double k3 = omega1 + stepSize * k2;
-    double theta1New = theta1 + (stepSize / 6.0) * (k0 + 2 * k1 + 2 * k2 + k3);
+    // All K4
+    double dTheta1_4 = omega1 + stepSize * dTheta1_3;
+    double dOmega1_4 = (-g * (2 * m1 + m2) * sin(theta1 + stepSize * dTheta1_3) - m2 * g * sin((theta1 + stepSize * dTheta1_3) - 2 * (theta2 + stepSize * dTheta2_3)) - 2 * sin((theta1 + stepSize * dTheta1_3) - (theta2 + stepSize * dTheta2_3)) * m2 * (pow(omega2 + stepSize * dOmega2_3, 2) * l2 + pow(omega1 + stepSize * dOmega1_3, 2) * l1 * cos((theta1 + stepSize * dTheta1_3) - (theta2 + stepSize * dTheta2_3)))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + stepSize * dTheta1_3) - 2 * (theta2 + stepSize * dTheta2_3))));
+    double dTheta2_4 = omega2 + stepSize * dTheta2_3;
+    double dOmega2_4 = (2 * sin((theta1 + stepSize * dTheta1_3) - (theta2 + stepSize * dTheta2_3)) * (pow(omega1 + stepSize * dOmega1_3, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1 + stepSize * dTheta1_3) + pow(omega2 + stepSize * dOmega2_3, 2) * l2 * m2 * cos((theta1 + stepSize * dTheta1_3) - (theta2 + stepSize * dTheta2_3)))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * (theta1 + stepSize * dTheta1_3) - 2 * (theta2 + stepSize * dTheta2_3))));
 
- 
-    // Calculate omega2
-    // these four variable are the K's of RK4
-    double t0 = (2 * sin(theta1 - theta2) * (pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + pow(omega2, 2) * l2 * m2 * cos(theta1 - theta2))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
 
-    double t1 = (2 * sin(theta1 - theta2) * (pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + pow((omega2 + stepSize * 0.5 * t0), 2) * l2 * m2 * cos(theta1 - theta2 ))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * (theta2 ))));
-
-    double t2 = (2 * sin(theta1 - theta2) * (pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + pow((omega2 + stepSize * 0.5 * t1), 2) * l2 * m2 * cos(theta1 - theta2))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * (theta2 ))));
-
-    double t3 = (2 * sin(theta1 - theta2) * (pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + pow((omega2 + stepSize * t2), 2) * l2 * m2 * cos(theta1 - theta2))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 *( theta2 ))));
-
-    double omega2New = omega2 + (stepSize / 6.0) * (t0 + 2 * t1 + 2 * t2 + t3);
-
-    
-    // Calculate theta2
-    // these four variable are the K's of RK4
-    double w0 = omega2;
-    double w1 = omega2 + stepSize * 0.5 * w0;
-    double w2 = omega2 + stepSize * 0.5 * w1;
-    double w3 = omega2 + stepSize * w2;
-    double theta2New = theta2 + (stepSize / 6.0) * (w0 + 2 * w1 + 2 * w2 + w3);
+    double theta1New = theta1 + (stepSize / 6.0) * (dTheta1_1 + 2 * dTheta1_2 + 2 * dTheta1_3 + dTheta1_4);
+    double omega1New = omega1 + (stepSize / 6.0) * (dOmega1_1 + 2 * dOmega1_2 + 2 * dOmega1_3 + dOmega1_4);
+    double theta2New = theta2 + (stepSize / 6.0) * (dTheta2_1 + 2 * dTheta2_2 + 2 * dTheta2_3 + dTheta2_4);
+    double omega2New = omega2 + (stepSize / 6.0) * (dOmega2_1 + 2 * dOmega2_2 + 2 * dOmega2_3 + dOmega2_4);
 
 
     // updating variables 
@@ -71,7 +65,6 @@ void DoublePendulum::update_RK4() {
 
 
 }
-
 void DoublePendulum::draw(Interface& interface) {
     interface.drawDP(m1, m2, l1, l2, theta1, theta2, pos);
 	
--- Program.cpp
+++ Program.cpp
@@ -1,16 +1,19 @@
 #include "Program.h"
 
-Program::Program(): dp(30.0, 15.0, 100.0, 150.0, pi / 2, pi / 2, 0, 0, 0.01, sf::Vector2f(400.0f, 300.0f)) {
-}
+Program::Program(): dp1(30.0, 15.0, 100.0, 150.0, pi/2.0, pi/2.0, 0, 0, 0.02, sf::Vector2f(400.0f, 300.0f))
+				   ,dp2(30.0, 15.0, 100.0, 150.0, pi/2.0, pi/2.0, 0, 0, 0.02, sf::Vector2f(400.0f, 300.0f))
+{}
 
 void Program::simulate() {
-	dp.update_RK4();
+	dp1.update_RK4();
+	dp2.update_RK4();
 
 }
 
 void Program::updateInterface(){
 	interface.control();
 	interface.clear();
-	dp.draw(interface);
+	dp1.draw(interface);
+	dp2.draw(interface);
 	interface.display();
 }
